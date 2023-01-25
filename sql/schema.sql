7d535cb70a7bfba6ea475583d0a6c341030d3939
liliaungurean <lilicikau95@gmail.com>
2023-01-24 20:36:27 -0600

    added in schema.js

--- sql/schema.sql
+++ sql/schema.sql
@@ -8,3 +8,20 @@ CREATE TABLE department (
     name VARCHAR(45) NOT NULL, 
     PRIMARY KEY (id)
 );
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT, 
+    title VARCHAR(45) NOT NULL, 
+    salary DECIMAL NOT NULL, 
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(45) NOT NULL,
+    last_name VARCHAR(45) NOT NULL,
+    role_id INT NOT NULL,
+    manager_name VARCHAR(45) NOT NULL,
+    PRIMARY KEY (id)
+)
